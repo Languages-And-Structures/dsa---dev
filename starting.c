53d005a02104a546809e20704f3a58e7f4206a31
VOID-3560 <dev.bhardwaj.3560@gmail.com>
2025-04-20 06:09:55 +0530

    going to start stacks and que

---
+++ starting.c
@@ -0,0 +1,7 @@
+// What is a Stack?
+// A Stack is a linear data structure that follows the LIFO principle — Last In, First Out.
+// Real-Life Analogy:
+// Think of a stack of plates:
+//     ->  You remove the top plate first.
+//     ->  You add plates at the top.
+// You can’t remove the bottom plate unless all the plates above it are removed first.
